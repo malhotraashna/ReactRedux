0985aa2ce901ab06dd499192e77e71c2fc412a2a
malhotraashna0 <malhotraashna0@gmail.com>
2018-03-01 12:04:36 +0530

    Updated

--- store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-import {reducers} from './reducer';
-
-export function configureStore(initialState = {}) {
-    const store = createStore(reducers, initialState);
-    return store;
-  }
-  
-  export const store = configureStore();
